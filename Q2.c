4938d57fef5cbb4857ba4b1d89b81272c1dd58bb
Muhammad Siddique <mrsiddique19@gmail.com>
2025-10-27 10:28:44 +0500

    Add files via upload

--- Q2.c
+++ Q2.c
@@ -1,25 +1,8 @@
 #include <stdio.h>
 
-float calculateTotal(float itemPrices[], int itemsNumber){
-	int i;
-	float totalCost = 0;
-	for(i = 0 ; i < itemsNumber ; i++){
-		totalCost += itemPrices[i];
-	}
-	return totalCost;
-}
-
-float applyDiscount(float totalCost){
-	if(totalCost > 5000){
-		float discount = totalCost*10/100;
-		printf("You Got A Discount Of %0.2f\n",discount);
-		totalCost = totalCost - discount;
-	}
-	return totalCost;
-}
-void printBill(float totalCost){
-	printf("The Total Bill Is %0.2f",totalCost);
-}
+float calculateTotal(float itemPrices[], int itemsNumber);
+float applyDiscount(float totalCost);
+void printBill(float totalCost);
 
 main(){
 	int itemsNumber = 6;
@@ -30,7 +13,32 @@ main(){
 		scanf("%f",&itemPrices[i]);
 	}
 	float total = calculateTotal(itemPrices , itemsNumber);
-	total = applyDiscount(total);
-	printBill(total);
+	float finalAmount = applyDiscount(total);
+	printBill(finalAmount);
 	 
 }
+
+
+float calculateTotal(float itemPrices[], int itemsNumber){
+	int i;
+	float totalCost = 0;
+	for(i = 0 ; i < itemsNumber ; i++){
+		totalCost += itemPrices[i];
+	}
+	return totalCost;
+}
+
+float applyDiscount(float totalCost){
+	float finalAmount;
+	if(totalCost > 5000){
+		float discount = totalCost*10/100;
+		printf("You Got A Discount Of %0.2f\n",discount);
+		finalAmount = totalCost - discount;
+			return finalAmount;
+	}
+	return totalCost;
+}
+void printBill(float finalAmount){
+	printf("The Total Bill Is %0.2f",finalAmount);
+}
+
